f9242069705c4b515ffe9fc16c8776bf7444708d
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-04-15 23:57:47 +0200

    Prettify our index.html site

--- src/ts/main_browser.ts
+++ src/ts/main_browser.ts
@@ -1,4 +1,5 @@
 var WebTorrent = require('webtorrent')
+var dragDrop = require('drag-drop')
 
 var client = new WebTorrent()
 
@@ -6,33 +7,44 @@ client.on('error', function (err:any) {
     console.error('ERROR: ' + err.message)
 })
 
-function onTorrent (torrent:any) {
-    log('Got torrent metadata!')
-        log(
-                'Torrent info hash: ' + torrent.infoHash + ' ' +
-                '<a href="' + torrent.magnetURI + '" target="_blank">[Magnet URI]</a> ' +
-                '<a href="' + torrent.torrentFileBlobURL + '" target="_blank" download="' + torrent.name + '.torrent">[Download .torrent]</a>'
-           )
+function print_torrent(torrent:any)
+{
+    log('<b>' + torrent.name + '</b>' +
+            '<ul>' +
+            '<li> hash: ' + torrent.infoHash + '</li> ' +
+            '<li> <a href="' + torrent.magnetURI + '" target="_blank">[Magnet URI]</a></li>' +
+            '<li> <a href="' + torrent.torrentFileBlobURL + '" target="_blank" download="' + torrent.name + '.torrent">[Download .torrent]</a></li>' +
+            '</ul>'
+       );
+}
 
-        // Print out progress every 5 seconds
-        var interval = setInterval(function () {
-            log('Progress: ' + (torrent.progress * 100).toFixed(1) + '%')
-        }, 5000)
+function onTorrent (torrent:any) {
+    log('Got torrent metadata!');
+    print_torrent(torrent);
+
+    // Print out progress every 5 seconds
+    var interval = setInterval(function () {
+        log('Progress: ' + (torrent.progress * 100).toFixed(1) + '%');
+    }, 5000)
 
     torrent.on('done', function () {
-        log('Progress: 100%')
-            clearInterval(interval)
+        log('Progress: 100%');
+        clearInterval(interval);
     })
 
     // Render all files into to the page
     torrent.files.forEach(function (file:any) {
-        file.appendTo('.log')
-            log('(Blob URLs only work if the file is loaded from a server. "http//localhost" works. "file://" does not.)')
-            file.getBlobURL(function (err:any, url:any) {
-                if (err) return log(err.message)
-                    log('File done.')
-                        log('<a href="' + url + '">Download full file: ' + file.name + '</a>')
-            })
+        file.appendTo('.log');
+        log('(Blob URLs only work if the file is loaded from a server. "http//localhost" works. "file://" does not.)');
+        file.getBlobURL(function (err:any, url:any)
+        {
+            if (err)
+            {
+                return log(err.message);
+            }
+            log('File done.');
+            log('<a href="' + url + '">Download full file: ' + file.name + '</a>');
+        })
     })
 }
 
@@ -45,23 +57,33 @@ document.querySelector('form').addEventListener('submit', function (e) {
     client.add(torrentId, onTorrent)
 })
 
+// When user drops files on the browser, create a new torrent and start seeding it!
+dragDrop('#droparea', function (files:any) {
+    log('User dropped file(s):');
+    var list = '<ul>';
+    files.forEach(function (file:any)
+    {
+        list += '<li>';
+        list += file.name;
+        list += '</li>';
+    });
+    list += '</ul>';
+    log(list);
+
+    log("Starting to seed them!");
+
+    client.seed(files, function (torrent:any)
+    {
+        print_torrent(torrent);
+    });
+})
+
+
 function log (str:any) {
     var p = document.createElement('p')
         p.innerHTML = str
         document.querySelector('.log').appendChild(p)
 }
 
-log("A");
-
-var dragDrop = require('drag-drop')
-
-// When user drops files on the browser, create a new torrent and start seeding it!
-dragDrop('#droparea', function (files:any) {
-    console.log('Here are the dropped files', files);
-    client.seed(files, function (torrent:any) {
-        console.log('Client is seeding:', torrent.infoHash);
-        console.log('magnet link:', torrent.magnetURI);
-        
-    })
-})
+//log("A");
 
