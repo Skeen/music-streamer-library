c87ebebfe869f40c56e05eefb8f822242f5b3804
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-04-19 17:00:29 +0200

    Initial work enabling our on testing framework

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var statics     = require('./build-tasks/statics');
 var browserify  = require('./build-tasks/browserify');
 var clean       = require('./build-tasks/clean');
 var node        = require('./build-tasks/node');
+var test        = require('./build-tasks/test');
 
 // Sub-tasks
 gulp.task('server:reload', "Reload the attached browsers", server.reload);
@@ -26,6 +27,9 @@ gulp.task('browserify:watch', ['typescript:watch'], browserify.watch);
 gulp.task('node:rerun', false, node.run);
 gulp.task('node:run', "Run cli", ['typescript:compile'], node.run);
 gulp.task('node:watch', ['typescript:watch'], node.watch);
+
+gulp.task('test:local', ['browserify:compile'], test.local);
+gulp.task('test:browser', ['browserify:compile'], test.browser);
 
 // Accumulative tasks
 gulp.task('compile', "Compile everything", [
@@ -49,3 +53,5 @@ gulp.task('browser', "Serve project watching for changes", ['serve', 'watch']);
 gulp.task('desktop', "Run cli and watch for changes", ['node:run', 'node:watch']);
 
 gulp.task('default', ["browser"]);
+
+gulp.task('test', ["test:local", "test:browser"]);
---
+++ test/local/test.js
@@ -0,0 +1,16 @@
+describe('nodejs.nopromises', function()
+{
+    beforeEach(function(done) {
+        done();
+    });
+
+    describe('Testing: Data API', function()
+    {
+        describe('Testing: setItem', function()
+        {
+            it('should save data (error check)', function(done) {
+                done();
+            });
+        });
+    });
+});
