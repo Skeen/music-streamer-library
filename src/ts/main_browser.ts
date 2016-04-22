e3994685bd1e83a2d24d90531fb2b247d839a1a4
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-04-22 13:53:59 +0200

    Added an explicit dht overlay file

--- src/ts/main_browser.ts
+++ src/ts/main_browser.ts
@@ -4,58 +4,9 @@ var mm = require('musicmetadata')
 
 var client = new WebTorrent()
 
-var replacer = function(key:any, value:any)
-{
-    // An artist has multiple albums
-	if (key == 'albums') {
-		var albums = value;
-		var sha1Albums:any = [];
-		for(var key in albums){
-			var album = albums[key];
-			sha1Albums.push(sha1(album.name));
-			console.log(album.name);
-		}
-		console.log(value);
-	  return sha1Albums;
-	}
-    // An album has multiple songs
-	if (key == 'songs') {
-		var songs = value;
-		var sha1Songs:any = [];
-		for(var key in songs){
-			var song = songs[key];
-			sha1Songs.push(sha1(song.title));
-			console.log(song.name);
-		}
-		console.log(value);
-	  return sha1Songs;
-	}
-    // Song and Album has multiple artists
-	if (key == 'artists') {
-		var artists = value;
-		var sha1Artists:any = [];
-		for(var key in artists){
-			var artist = artists[key];
-			sha1Artists.push(sha1(artist.name));
-			console.log(artist.name);
-		}
-		console.log(value);
-	  return sha1Artists;
-	}
-    // Song has an album
-	if (key == 'album') {
-		console.log(value);
-		var sha1Album = sha1(value);
-	  return sha1Album;
-	}
-	return value;
-}
-
-
-
 import { HashTable, HTTP_HashTable } from "./dht";
 import { sha1 } from './sha1';
-import { Song, Album, Artist } from './music';
+import { addSong } from './dht_overlay';
 
 // TODO: Replace by 'new Distributed_HashTable();'
 var hash_table:HashTable = new HTTP_HashTable();
@@ -135,7 +86,7 @@ document.querySelector('#search').addEventListener('submit', function (e) {
             return;
         }
 
-        log("Found value: " + value, null, 4));
+        log("Found value: " + value);
         console.log(JSON.stringify(JSON.parse(value), null, 4));
 
     });
@@ -164,54 +115,7 @@ dragDrop('#droparea', function (files:any) {
                 if (err) throw err;
                 log(JSON.stringify(metadata, null, 4));
 
-                // We'll add album and artist later
-                var song = new Song(metadata.title, JSON.stringify(metadata.genre), metadata.year, metadata.duration, null, null, torrent.magnetURI);
-
-                // We'll add artist later
-                var album = new Album(metadata.album, [song], null);
-
-                var artists:Artist[] = [];
-                for(var key in metadata.artist)
-                {
-                    var meta_artist = metadata.artist[key];
-                    var artist = new Artist(meta_artist, [album]);
-                    artists.push(artist);
-                }
-
-                // Setup references
-                album.setArtist(artists);
-                song.setArtist(artists);
-                song.setAlbum(album);
-
-                hash_table.put(artist.getName(), JSON.stringify({type: "artist", payload: artist}, replacer), function(err, value)
-                {
-                    if(err)
-                    {
-                        log("Unable to store in overlay network: " + err.message);
-                        return;
-                    }
-                    log("Stored in overlay network: " + value);
-                });
-
-                hash_table.put(album.getName(), JSON.stringify({type: "album", payload: album}, replacer), function(err, value)
-                {
-                    if(err)
-                    {
-                        log("Unable to store in overlay network: " + err.message);
-                        return;
-                    }
-                    log("Stored in overlay network: " + value);
-                });
-
-                hash_table.put(song.getTitle(), JSON.stringify({type: "song", payload: song}, replacer), function(err, value)
-                {
-                    if(err)
-                    {
-                        log("Unable to store in overlay network: " + err.message);
-                        return;
-                    }
-                    log("Stored in overlay network: " + value);
-                });
+                addSong(metadata, torrent.magnetURI, hash_table, log);
             });
 
             print_torrent(torrent);
