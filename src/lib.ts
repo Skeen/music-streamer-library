4cc30b881934def25faf0d0492a2eb987a9d580d
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-05-14 13:29:57 +0200

    Library is ready to use!

--- src/lib.ts
+++ src/lib.ts
@@ -1,9 +1,6 @@
-export * from "./chord-dht.ts";
-export * from "./dht_overlay.ts";
-export * from "./dht.ts";
-export * from "./music.ts";
-export * from "./sha1.ts";
-export * from "./storage.ts";
-export * from "./torrent.ts";
-
-export var skeen_hest = "WOOO!";
+export * from "./dht_overlay";
+export * from "./dht";
+export * from "./music";
+export * from "./sha1";
+export * from "./storage";
+export * from "./torrent";
